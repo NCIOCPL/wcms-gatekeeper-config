91f6a7193c4eedb2c3be32c4339aef17fd1ed68a
Blair <blairlearn@users.noreply.github.com>
2018-03-07 17:09:40 -0500

    Pre-create directories
    Guarantee target directories exist for all generated config files.
    Redirect stderr to hide messages for pre-existing directories.

--- tools/generate.bat
+++ tools/generate.bat
@@ -21,35 +21,39 @@ pushd %~dp0 && cd ..
 REM GateKeeper apps
 for %%a in (Admin CDRPreviewWS WebSvc) do (
     echo Generating '%GATEKEEPER_ROOT%\App\%%a\Web.config'
+    mkdir "%GATEKEEPER_ROOT%\App\%%a" 2> nul
     powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile %%a\Web.config -OutputFile '%GATEKEEPER_ROOT%\App\%%a\Web.config' -SubstituteList '%GATEKEEPER_SUBST%'
 )
 echo Generating '%GATEKEEPER_ROOT%\App\ProcMgr\App.config'
+mkdir "%GATEKEEPER_ROOT%\App\ProcMgr" 2> nul
 powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile ProcMgr\App.config -OutputFile '%GATEKEEPER_ROOT%\App\ProcMgr\App.config' -SubstituteList '%GATEKEEPER_SUBST%'
 
 REM Test harnesses.
 for %%a in (PromotionTester ImportTool ProcessTester PubPreviewTest) do (
     echo Generating '%GATEKEEPER_ROOT%\Test Harnesses\%%a\App.config'
+    mkdir "%GATEKEEPER_ROOT%\Test Harnesses\%%a\App.config" 2> nul
     powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile %%a\App.config -OutputFile '%GATEKEEPER_ROOT%\Test Harnesses\%%a\App.config' -SubstituteList '%GATEKEEPER_SUBST%'
 )
 echo Generating '%GATEKEEPER_ROOT%\Test Harnesses\UnitTest\UnitTest.dll.config'
+mkdir "%GATEKEEPER_ROOT%\Test Harnesses\UnitTest" 2> nul
 powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile UnitTest\UnitTest.dll.config -OutputFile '%GATEKEEPER_ROOT%\Test Harnesses\UnitTest\UnitTest.dll.config' -SubstituteList '%GATEKEEPER_SUBST%'
 
 REM GateKeeperTest config
 echo Generating '%GATEKEEPER_ROOT%\Test Harnesses\GateKeeperTest\GateKeeperTest.exe.config'
+mkdir "%GATEKEEPER_ROOT%\Test Harnesses\GateKeeperTest" 2> nul
 powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile GateKeeperTest\GateKeeperTest.exe.config -OutputFile '%GATEKEEPER_ROOT%\Test Harnesses\GateKeeperTest\GateKeeperTest.exe.config' -SubstituteList '%GATEKEEPER_SUBST%'
 
 REM database connections
 for %%a in (Admin CDRPreviewWS WebSvc ProcMgr) do (
     echo Generating '%GATEKEEPER_ROOT%\App\%%a\sharedconfig\connectionStrings.config'
-    mkdir "%GATEKEEPER_ROOT%\App\%%a\sharedconfig\"
+    mkdir "%GATEKEEPER_ROOT%\App\%%a\sharedconfig\" 2> nul
     powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile sharedconfig\connectionStrings.config -OutputFile '%GATEKEEPER_ROOT%\App\%%a\sharedconfig\connectionStrings.config' -SubstituteList '%GATEKEEPER_SUBST%'
 )
 for %%a in (UnitTest PromotionTester) do (
     echo Generating '%GATEKEEPER_ROOT%\Test Harnesses\%%a\sharedconfig\connectionStrings.config'
-    mkdir "%GATEKEEPER_ROOT%\Test Harnesses\%%a\sharedconfig\"
+    mkdir "%GATEKEEPER_ROOT%\Test Harnesses\%%a\sharedconfig\" 2> nul
     powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile sharedconfig\connectionStrings.config -OutputFile '%GATEKEEPER_ROOT%\Test Harnesses\%%a\sharedconfig\connectionStrings.config' -SubstituteList '%GATEKEEPER_SUBST%'
 )
-
 
 REM Return to original directory.
 popd
