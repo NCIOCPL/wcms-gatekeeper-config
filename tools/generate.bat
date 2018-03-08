663135f4a895e89f23e19506a4b7c1f92de012e2
Blair Learn <blairlearn@users.noreply.github.com>
2018-03-08 13:16:54 -0500

    Remove sharedconfig directory for connectionStrings.

--- tools/generate.bat
+++ tools/generate.bat
@@ -63,14 +63,14 @@ IF '%DEPLOY_ONLY%' == 'false' (
 
     REM database connections
     for %%a in (Admin CDRPreviewWS WebSvc ProcMgr) do (
-        echo Generating '%GATEKEEPER_ROOT%\App\%%a\sharedconfig\connectionStrings.config'
-        mkdir "%GATEKEEPER_ROOT%\App\%%a\sharedconfig\" 2> nul
-        powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile sharedconfig\connectionStrings.config -OutputFile '%GATEKEEPER_ROOT%\App\%%a\sharedconfig\connectionStrings.config' -SubstituteList '%GATEKEEPER_SUBST%'
+        echo Generating '%GATEKEEPER_ROOT%\App\%%a\connectionStrings.config'
+        mkdir "%GATEKEEPER_ROOT%\App\%%a\" 2> nul
+        powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile sharedconfig\connectionStrings.config -OutputFile '%GATEKEEPER_ROOT%\App\%%a\connectionStrings.config' -SubstituteList '%GATEKEEPER_SUBST%'
     )
     for %%a in (UnitTest PromotionTester) do (
-        echo Generating '%GATEKEEPER_ROOT%\Test Harnesses\%%a\sharedconfig\connectionStrings.config'
-        mkdir "%GATEKEEPER_ROOT%\Test Harnesses\%%a\sharedconfig\" 2> nul
-        powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile sharedconfig\connectionStrings.config -OutputFile '%GATEKEEPER_ROOT%\Test Harnesses\%%a\sharedconfig\connectionStrings.config' -SubstituteList '%GATEKEEPER_SUBST%'
+        echo Generating '%GATEKEEPER_ROOT%\Test Harnesses\%%a\connectionStrings.config'
+        mkdir "%GATEKEEPER_ROOT%\Test Harnesses\%%a\" 2> nul
+        powershell -ExecutionPolicy Unrestricted tools/substitution.ps1 -InputFile sharedconfig\connectionStrings.config -OutputFile '%GATEKEEPER_ROOT%\Test Harnesses\%%a\connectionStrings.config' -SubstituteList '%GATEKEEPER_SUBST%'
     )
 )
 
